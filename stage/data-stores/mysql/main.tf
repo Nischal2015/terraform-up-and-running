04fbf6b9ee4abcc4b72eaeb471347758b6aa74b6
Nischal Shakya <nischalshakya2015@gmail.com>
2023-06-07 22:17:02 +0545

    Add RDS MySQL database

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket-nischal"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-west-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
